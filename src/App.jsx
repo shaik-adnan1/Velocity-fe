53cd33a6cd9cde4c7fe41e1e3f1ade2f3429aa1a
shaik-adnan1 <yousufwrk0@gmail.com>
2024-09-21 15:10:20 +0530

    initialized Framer motion and setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BasicFM } from "./components/motion-components/basicFM";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,6 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <BasicFM />
     </>
   );
 }
