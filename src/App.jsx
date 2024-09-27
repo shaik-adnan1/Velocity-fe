83b29e32f95d91e86d46b0498540b71075c1d6f2
shaik-adnan1 <yousufwrk0@gmail.com>
2024-09-27 11:20:41 +0530

    test commit with college wifi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
+// import React from "react";
 import { BasicFM } from "./components/motion-components/basicFM";
 import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
+import { CircularProgress } from "@nextui-org/progress";
 
 function App() {
+  // const [value, setValue] = React.useState(0);
+
+  // React.useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setValue((v) => (v >= 100 ? 0 : v + 10));
+  //   }, 500);
+
+  //   return () => clearInterval(interval);
+  // }, []);
   return (
     <>
       <AvatarGroup className="flex flex-col gap-2">
@@ -21,6 +32,14 @@ function App() {
       </p>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
       <BasicFM />
+      <CircularProgress
+      label="Speed"
+      size="lg"
+      value={70}
+      color="success"
+      formatOptions={{ style: "unit", unit: "kilometer" }}
+      showValueLabel={true}
+    />
     </>
   );
 }
